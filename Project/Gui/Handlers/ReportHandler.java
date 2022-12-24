7305695072398296b148a6e6e304edb9996c4600
utkuuci <muzaffertolgayakar@gmail.com>
2022-12-25 02:23:15 +0300

    report ip filtered eklendi

--- Project/Gui/Handlers/ReportHandler.java
+++ Project/Gui/Handlers/ReportHandler.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -32,13 +34,25 @@ public class ReportHandler extends HandlerRepository<ReportHandler>{
 
             String line = "";
             String result = "";
+            String ip = JOptionPane.showInputDialog("Enter the IP");
+            List<String> filteredIp = new ArrayList<>();
             while((line = reader.readLine()) != null) {
                 result += line + "\n";
+                if(line.contains(ip)){
+                    filteredIp.add(line);
+                }
             }
-
+            System.out.println(result);
+            String result2 = "";
+            for(int i = 0; i < filteredIp.size(); i++) {
+                result2 += filteredIp.get(i) + "\n";
+            }
+            if(result2.equals(new String())) {
+                result2 = "No data!!";
+            }
             ReadFileService readFileService = CoreFactory.creatReadFileService(new File(Paths.get("Project", "Gui", "gui.log").toAbsolutePath().toString()));
             String guiLogs = readFileService.getTExt();
-            JOptionPane.showMessageDialog(null ,result, "Reports(Proxy Logs)", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null ,result2, "Reports(Proxy Logs)", JOptionPane.INFORMATION_MESSAGE);
             JOptionPane.showMessageDialog(null ,guiLogs, "Reports(Gui Logs)", JOptionPane.INFORMATION_MESSAGE);
             socket.close();
         } catch(Exception ex) {
